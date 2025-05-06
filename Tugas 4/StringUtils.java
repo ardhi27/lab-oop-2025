6875d2d6b4e42e9d050d343edff63b6218db3576
jabbarfanshurna <jabbarfanshurna@gmail.com>
2025-05-06 14:04:13 +0800

    kumpul tugas praktikum 4

---
+++ Tugas 4/StringUtils.java
@@ -0,0 +1,6 @@
+public class StringUtils {
+    public static String generateNickName(String fullName) {
+        String[] parts = fullName.trim().split(" ");
+        return parts[parts.length - 1];
+    }
+}
