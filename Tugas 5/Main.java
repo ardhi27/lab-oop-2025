276ae09bff3c7e1b21c3d1f909ab63d55f5d5564
HA141User <muhammadhairi0123@gmail.com>
2025-05-09 15:56:07 +0800

    Commit tugas 6

--- Tugas 5/Main.java
+++ Tugas 5/Main.java
@@ -29,7 +29,8 @@ public class Main {
             
         }
 
-        sc.close();    }
+        sc.close();    
+    }
 
 
     
---
+++ Tugas 6/Main.java
@@ -0,0 +1,8 @@
+import Models.*;
+
+public class Main{
+    public static void main(String[] args){
+        Mobil mobil = new Mobil("Yamaha", "Kijang");
+        
+    }
+}
